2ae8ff7bd738b75e75eda574f4c31d0dbc265348
crsrafael <carlosrafael_silva@yahoo.com.br>
2021-12-04 12:36:09 -0300

    Inclusão de data annotations para validar as propriedades das classes ContatosModel e ConvidadosModel.

--- BonoboEventos.Webapi/Model/ContatoModel.cs
+++ BonoboEventos.Webapi/Model/ContatoModel.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bonobo.Model
 {
     public class ContatoModel
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Informe o tipo do contato")]
+        [MaxLength(50, ErrorMessage ="Máximo de 50 caracteres")]
         public string Tipo { get; set; }
 
+        [Required(ErrorMessage = "Informe o contato")]
+        [MaxLength(50, ErrorMessage ="Máximo de 50 caracteres")]
         public string Contato { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "Informe o Convidado")]
         public int ConvidadoId { get; set; }
     }
 }
--- BonoboEventos.Webapi/Model/ConvidadoModel.cs
+++ BonoboEventos.Webapi/Model/ConvidadoModel.cs
@@ -1,15 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bonobo.Model
 {
     public class ConvidadoModel
     {
         public int Id {get; set;}
-
+        
+        [Required(ErrorMessage = "Informe o nome do convidado")]
+        [MaxLength(100, ErrorMessage = "Máximo de 100 caracteres")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Informe o apelido do convidado")]
+        [MaxLength(50, ErrorMessage = "Máximo de 50 caracteres")]
         public string Apelido { get; set; }
 
+        [Required(ErrorMessage = "Informe data de nascimento do convidado")]
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
         public DateTime DataDeNascimento { get; set; }
     }
 }
